1a9059c4f0c6567982eb60f49f0b560f3fa56fb7
abdo-elbeltagy <abdo.elbeltagy401@gmail.com>
2019-12-05 13:16:34 +0200

    added login and register forms

---
+++ client/viewmodels/login.js
@@ -0,0 +1,31 @@
+
+function GenericLoginViewModel(url) {
+  return function () {
+    let self = this
+    self.email = ko.observable();
+    self.password = ko.observable();
+    self.login = () => {
+      fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          email: self.email(),
+          password: self.password(),
+        })
+      }).then(response =>
+        response.json()
+      ).then((data) => {
+        console.log('Request success: ', data);
+        localStorage.setItem("authData", data.authData)
+        localStorage.setItem("userData", JSON.stringify(data.userData))
+      }).catch(function (err) {
+        console.log(err)
+      })
+    };
+  };
+}
+const DoctorLoginViewModel = GenericLoginViewModel("https://localhost:5000/api/doctors/login");
+// const UserLoginViewModel = GenericLoginViewModel("http://localhost:5000/api/doctors/login");
+
+ko.applyBindings(new DoctorLoginViewModel(), document.getElementById("doctorLoginId"));
+// ko.applyBindings(new UserLoginViewModel(), document.getElementById("userLoginId"));
