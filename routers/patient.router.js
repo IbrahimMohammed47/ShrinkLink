4bd85e9065106a2a63635fedfabab4fe719537a6
darknsSs512 <msapry007@gmail.com>
2019-12-05 21:44:48 +0200

    just stripe

--- routers/patient.router.js
+++ routers/patient.router.js
@@ -3,9 +3,18 @@ const bcrypt = require('bcryptjs');
 const router = express.Router()
 
 const { Patient, Report } = require('../models/')
+const stripe = require('stripe')('sk_test_Aqa9dsgiQPVXBfyw9fTgJA5h00LK5cUlKs');
 router.post('/create', async (req, res) => {
-
   try {
+
+    const customer = await stripe.customers.create({
+      name: req.body.firstName + " " + req.body.lastName,
+      email: req.body.email,
+      description: 'sapry dummy description'
+    });
+
+
+
     let patient = await Patient.create({
       firstName: req.body.firstName,
       lastName: req.body.lastName,
@@ -14,6 +23,7 @@ router.post('/create', async (req, res) => {
       mobileNumber: req.body.mobileNumber,
       age: req.body.age,
       gender: req.body.gender,
+      stripe_id: customer.id,
     })
     res.send(`patient created with id ${patient.id}`)
   } catch (error) {
